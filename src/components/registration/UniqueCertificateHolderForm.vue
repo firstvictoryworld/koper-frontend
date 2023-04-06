af65115cf37998dd0f305d14d7873766a99ab3ec
Suraj Shrestha <sunlight.suraj5@gmail.com>
2023-04-06 15:27:16 +0545

    Code ficale validation

--- src/components/registration/UniqueCertificateHolderForm.vue
+++ src/components/registration/UniqueCertificateHolderForm.vue
@@ -4,7 +4,7 @@
 			<v-col v-for="key in keys" :key="key" cols="12" md="4">
 				<v-text-field v-bind:model-value="data[key]" hide-details="auto" color="koperniko-secondary"
 					variant="outlined" :type="key == 'unique_certificate_holder_birth_date' ? 'date' : 'text'" :label="$t(`registration.bankAccount.${key}`)"
-					:rules="[]"
+					:rules="fieldRules[key] || []"
 					@update:modelValue="(value: string) => $emit('update:data', { ...data, [key]: value })"
 					:disabled="!!props.readonly" />
 			</v-col>
@@ -14,10 +14,10 @@
 
 <script setup lang="ts">
 import CardContainer from '@/components/common/CardContainer.vue'
-import { requiredValidation } from '@/validation/rules';
+import { codFiscaleOrPivaValidation, requiredValidation, type ValidationRule } from '@/validation/rules';
 import { computed } from 'vue';
 
-interface Record {
+interface RecordInterface {
 	unique_certificate_holder_name: string
 	unique_certificate_holder_surname: string
 	unique_certificate_holder_fiscal_code: string
@@ -26,10 +26,18 @@ interface Record {
 }
 
 interface Props {
-	data: Record
+	data: RecordInterface
 	readonly?: boolean
 }
 
 const props = defineProps<Props>()
-const keys = computed(() => Object.keys(props.data) as (keyof Record)[])
+const keys = computed(() => Object.keys(props.data) as (keyof RecordInterface)[])
+
+const fieldRules: Record<keyof RecordInterface, ValidationRule[]> = {
+	unique_certificate_holder_name: [],
+	unique_certificate_holder_surname: [],
+	unique_certificate_holder_fiscal_code: [requiredValidation, codFiscaleOrPivaValidation],
+	unique_certificate_holder_birth_place: [],
+	unique_certificate_holder_birth_date: [],
+}
 </script>
